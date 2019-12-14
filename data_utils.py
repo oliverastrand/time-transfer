6786393b39f1b2a7af5235cec940a715fd6f8b2e
Ruoyi Wang <endvroy@gmail.com>
2019-12-13 22:02:42 -0500

    reformatted and imported u-net

--- data_utils.py
+++ data_utils.py
@@ -5,24 +5,25 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import torchvision.transforms as transforms
 
+
 class TimedImageDataset(Dataset):
     def __init__(self, root_dir):
         self.root_dir = root_dir
         self.n = len([d for d in os.listdir(root_dir) if "." not in d])
         self.transform = transforms.Compose([
-                            transforms.CenterCrop(10),
-                            transforms.ToTensor(),
-                            ])
+            transforms.CenterCrop(10),
+            transforms.ToTensor(),
+        ])
 
     def __len__(self):
         return self.n
-                                            
+
     def __getitem__(self, idx):
         scene_dir = self.root_dir + f"/{idx}"
         times = [f[:-4] for f in os.listdir(scene_dir) if ".jpg" in f]
 
         scene_dict = {}
         for t in times:
-             image = Image.open(scene_dir+f"/{t}.jpg")
-             scene_dict[t] = self.transform(image)
+            image = Image.open(scene_dir + f"/{t}.jpg")
+            scene_dict[t] = self.transform(image)
         return scene_dict
