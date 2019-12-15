e2862ed3eefe61ed579df3fb0f238a30d9e5614e
ruoyi <endvroy@gmail.com>
2019-12-15 01:31:20 -0500

    successfully injected time as additional feature

--- models.py
+++ models.py
@@ -1,79 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
-        super().__init__()
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.double_conv(x)
-
-
-class DownNet(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
-        )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-
-class UpNet(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=False):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
-
-        self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-
-
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-""" Full assembly of the parts to form the complete network """
+import numpy as np
 
 
 class UNet(nn.Module):
@@ -109,12 +37,10 @@ class UNet(nn.Module):
                                    nn.ReLU(True),
                                    nn.InstanceNorm2d(hidden_dim * 16))
 
-        # self.down5 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=(4, 4), stride=2),
-        #                            nn.Conv2d(512, 512, kernel_size=(4, 4)),
-        #                            nn.InstanceNorm2d(512))
+        # self.dense = nn.Linear(hidden_dim * 16 * 25 * 25 + 2, hidden_dim * 16 * 25 * 25)
 
         self.up1 = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-                                 nn.Conv2d(hidden_dim * 32, hidden_dim * 8, kernel_size=(4, 4), padding=2),
+                                 nn.Conv2d(hidden_dim * 16 + 2, hidden_dim * 8, kernel_size=(4, 4), padding=2),
                                  nn.ReLU(True),
                                  nn.Conv2d(hidden_dim * 8, hidden_dim * 8, kernel_size=(4, 4), padding=1),
                                  nn.ReLU(True),
@@ -141,7 +67,12 @@ class UNet(nn.Module):
     def combine(self, x1, x2):
         return torch.cat([x1, x2], dim=1)
 
-    def forward(self, x):
+    def encode_time(self, t):
+        angle = 2 * np.pi / 24 * t
+        angle = angle.unsqueeze(-1)
+        return torch.cat([angle.cos(), angle.sin()], dim=1)
+
+    def forward(self, x, t):
         # downsampling
         print(x.shape)
         x1 = self.down1(x)
@@ -153,8 +84,12 @@ class UNet(nn.Module):
         x4 = self.down4(x3)
         print(x4.shape)
 
+        # x = x4.view((x4.size(0), -1))
+        # print(x.shape)
+        encoded_t = self.encode_time(t)
+        time_features = encoded_t.unsqueeze(0).repeat(1, 1, 25*25).view(-1, 2, 25, 25)
         # upsampling
-        x = self.up1(self.combine(x4, x4))
+        x = self.up1(self.combine(x4, time_features))
         print(x.shape)
         x = self.up2(self.combine(x, x3))
         print(x.shape)
@@ -170,4 +105,5 @@ class UNet(nn.Module):
 if __name__ == '__main__':
     unet = UNet(3, 16)
     example = torch.zeros((4, 3, 800, 450))
-    out = unet(example)
+    t = torch.zeros(4)
+    out = unet(example, t)
