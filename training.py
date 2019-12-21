604f543172eac83bf13e85597b39d6d59c3d48f9
Oliver Åstrand <oliver.astrand@gmail.com>
2019-12-20 20:29:19 -0500

    Cleaned up directory  for submission

--- training.py
+++
@@ -1,7 +0,0 @@
-from pytorch_lightning import Trainer
-from lightning import DumbSystem
-
-model = DumbSystem()
-
-trainer = Trainer(max_nb_epochs=3, gpus=1, early_stop_callback=None)
-trainer.fit(model) 
